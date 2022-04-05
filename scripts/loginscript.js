f7681091b6407acfd028d1aa69e0ecab1071db9a
codeironside <fury25423@gamil.com>
2022-04-05 19:27:44 -0300

    first robot sample

---
+++ scripts/loginscript.js
@@ -0,0 +1,9 @@
+const labels = document.querySelectorAll('.form_control label')
+labels.forEach(label =>{
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter,idx) => 
+    `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`)
+    // ms means milliSeconds
+    .join('')
+})
