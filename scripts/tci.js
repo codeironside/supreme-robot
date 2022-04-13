eb3c3a173e193ea67ce66e06550def4c40f8e8c6
codeironside <fury25423@gmail.com>
2022-04-13 14:07:25 +0100

    here comes another one

--- scripts/tci.js
+++ scripts/tci.js
@@ -1,35 +1,46 @@
-
-document.getElementById("submit_btn").addEventListener('click', (e) =>{
+// function buttonclick()
+document.getElementById("submit-btn").addEventListener('click',(e) =>{
     e.preventDefault();
-    
+
     const AFname = document.getElementById("Afname").value
-    const aircraftNumber = document.getElementById("AFnumber").value
+    const aircraftNumber = document.getElementById("Afnumber").value
     const country = document.getElementById("country").value
     const cUnit = document.getElementById("Cunit").value
     const NOcrew = document.getElementById("NOcrew").value
     const TOaircraft = document.getElementById("TOaircraft").value
     const Acallsign = document.getElementById("Acallsign").value
     const chieftech = document.getElementById("chieftech").value
-    const dates = document.getElementById("dates").value
-    const dates1 = document.getElementById("dates1").value
+    // const dates = document.getElementById("dates").value
+    const dates1 = document.getElementById("dates1")
     const flighthours = document.getElementById("flighthours").value
     const file = document.getElementById("file").value
-    const date = document.getElementById("date").value
+    const daten = document.getElementById("date").value
     const Tcapacity = document.getElementById("Tcapacity").value
     const TCIno = document.getElementById("TCIno").value
-    const dateprocured = document.getElementById("dateprocured").value
-    if((firstName === username1 || firstName === username2) && userPassword === password){
-
-        alert(`welcome ${firstName}, you are only signed in for one session`)
-        window.location.href="http://127.0.0.1:5500/html/services.html"
-    }
-    else{
-        const userChoice=confirm(`${firstName} is not found,would you like to register`)
-        if (userChoice === true){
-            window.location.href="http://127.0.0.1:5500/html/register.html"
+    const dateprocured = document.getElementsByName("dateprocured")
+    const radioButtons = document.getElementsByName("serviceable")
+    // querySelectorAll('.btn-radio')
+    // querySelectorAll('input[name="servicabe"]');
+    let Newname = document.getElementById("airName").textContent= AFname
+    console.log(AFname)
+    for(let i = 0; i < radioButtons.length; i++){
+        
+        if(radioButtons[i].checked){
+            alert(`are you sure this aircarft is ${radioButtons[i].value}, if not ask ask an engineer`)
+            console.log(radioButtons[i].value)
+            break
+        
+            
         }
         else{
-            window.location.href="http://127.0.0.1:5500/html/index.html"
+            alert("you havent chosen the status of this aircraft")
+            console.log(dateprocured)
+            break
+            
         }
+        
     }
-})
+   
+ 
+ })
+ 
