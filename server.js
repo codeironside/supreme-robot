4ece443faa63870624ca2df0821763b6f6c9c06e
codeironside <fury25423@gmail.com>
2022-05-17 20:03:03 +0100

    new message

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path')
 const app = express()
 const port = 8000
 
-app.use(express.static(path.join(__dirname,'./css')))
+app.use(express.static(path.join(__dirname,'./css/loginsytle.css')))
+app.use(express.static(path.join(__dirname,'./scripts/loginscript.js')))
 app.use('',(req,res,next)=>{
 res.sendFile(path.join(__dirname,'./html','index.html'));
 });
