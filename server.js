6f621ff4fddd94d3b2c991d9631867a77e96f398
codeironside <fury25423@gmail.com>
2022-05-17 18:29:23 +0100

    Revert "Dot branch"

--- server.js
+++ server.js
@@ -3,25 +3,10 @@ const path = require('path')
 const app = express()
 const port = 8000
 
-app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, 'html')); //use html directory as default view folder
-
-app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/css', express.static(path.join(__dirname, 'css')));
-
-app.get('/', (req, res) => {
-    res.render('index'); 
-})
-
-app.get('/about', (req, res) => {
-    res.render('About'); 
-    //Replace all ../ referencing files or images with a single / or none at all since static file paths have been set
-    //It's not necessary, but to be on the safe side.
-})
-
-
+app.use(express.static(path.join(__dirname,'./css')))
+app.use('',(req,res,next)=>{
+res.sendFile(path.join(__dirname,'./html','index.html'));
+});
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
 });
